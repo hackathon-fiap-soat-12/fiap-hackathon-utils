23b77500529a9fee5fd5a4fcd634f99dabfef88e
Wilton Souza <wiltonsouzafilho@yahoo.com.br>
2025-04-05 21:35:17 -0300

    Adicionando scripts de deploy, destroy e env vars

---
+++ destroy.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR="$(realpath "$(dirname "$0")")"
+
+source ./env_variables.sh
+
+
+run_terraform() {
+    local folder_path=$1
+    local abs_folder="$BASE_DIR/$folder_path"
+
+    echo "Running terraform destroy in $abs_folder"
+
+    terraform -chdir="$abs_folder" destroy --auto-approve || {
+        echo "Terraform destroy failed in $abs_folder"
+        exit 1
+    }
+
+    echo ""
+    echo ""
+    echo "Terraform destroy completed in $abs_folder"
+    echo "############################################"
+    echo "######### FINISHED TERRAFORM destroy #########"
+    echo "############################################"
+    echo ""
+    echo ""
+}
+
+# run_terraform "payment/terraform"
+# run_terraform "fiap-hackathon-video/terraform"
+# run_terraform "fiap-hackathon-mail/terraform"
+# run_terraform "fiap-hackathon-process/terraform"
+# run_terraform "fiap-hackathon-api-gateway/terraform"
+# run_terraform "fiap-hackathon-cognito/terraform"
+# run_terraform "fiap-hackathon-lambda-auth/terraform"
+run_terraform "fiap-hackathon-lambda-video-receive/terraform"
+run_terraform "fiap-hackathon-observability/terraform"
+run_terraform "fiap-hackathon-k8s-infra/terraform"
+run_terraform "fiap-hackathon-k8s-cluster/terraform"
+run_terraform "fiap-hackathon-queue/terraform"
+run_terraform "fiap-hackathon-db/terraform"
+run_terraform "fiap-hackathon-vpc/terraform"
+
+echo "All terraform destroy commands have completed."
+
+
+
+
+
+
+
+
+
+
+
---
+++ env_variables.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define the AWS credentials file path
+AWS_CREDENTIALS_FILE="$HOME/.aws/credentials"
+
+PROFILE="default"
+
+if [ ! -f "$AWS_CREDENTIALS_FILE" ]; then
+    echo "AWS credentials file not found: $AWS_CREDENTIALS_FILE"
+    exit 1
+fi
+
+TF_VAR_aws_access_key_id=$(grep -A3 "^\[$PROFILE\]" "$AWS_CREDENTIALS_FILE" | grep "aws_access_key_id" | sed 's/aws_access_key_id *= *//')
+TF_VAR_aws_secret_access_key=$(grep -A3 "^\[$PROFILE\]" "$AWS_CREDENTIALS_FILE" | grep "aws_secret_access_key" | sed 's/aws_secret_access_key *= *//')
+TF_VAR_aws_session_token=$(grep -A3 "^\[$PROFILE\]" "$AWS_CREDENTIALS_FILE" | grep "aws_session_token" | sed 's/aws_session_token *= *//')
+
+if [[ -n "$TF_VAR_aws_access_key_id" && -n "$TF_VAR_aws_secret_access_key" && -n "$TF_VAR_aws_session_token" ]]; then
+    export TF_VAR_aws_access_key_id TF_VAR_aws_secret_access_key TF_VAR_aws_session_token
+    echo "AWS credentials exported for profile '$PROFILE'."
+else
+    echo "Failed to extract AWS credentials for profile '$PROFILE'."
+    exit 1
+fi
+
+TF_VAR_sonarqube_db_name="sonarqube"
+TF_VAR_sonarqube_db_username="postgres"
+TF_VAR_sonarqube_db_password="postgres"
+
+export TF_VAR_sonarqube_db_name TF_VAR_sonarqube_db_username TF_VAR_sonarqube_db_password
+
+echo "Sonarqube postgres DB credentials exported."
